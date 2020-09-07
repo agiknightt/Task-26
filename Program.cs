2ab5a4e8ea569db4344b903282968c0318c7f1e6
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-09-08 01:02:46 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -13,36 +13,31 @@ namespace Task_26
 
             while (enterOrExit)
             {
-                InputAndSumNumbers(numbers, ref enterOrExit);
+                int sum = 0;
+
+                Console.WriteLine("Выберите нужный пункт меню\n\nadd - добавить число\n\nsum - вывести сумму введеных чисел\n\nexit - выход\n");
+
+                switch (Console.ReadLine())
+                {
+                    case "add":
+                        Console.Write("Введите число : ");
+                        numbers.Add(Convert.ToInt32(Console.ReadLine()));
+                        break;
+                    case "sum":
+                        for (int i = 0; i < numbers.Count; i++)
+                        {
+                            sum += numbers[i];
+                        }
+                        Console.WriteLine(sum);
+                        break;
+                    case "exit":
+                        enterOrExit = false;
+                        break;
+                }
+                Console.WriteLine("Нажмите любую клавишу для продолжения...");
+                Console.ReadKey();
+                Console.Clear();
             }
-        }
-
-        private static void InputAndSumNumbers(List<int> numbers, ref bool enterOrExit)
-        {
-            int sum = 0;
-
-            Console.WriteLine("Выберите нужный пункт меню\n\nadd - добавить число\n\nsum - вывести сумму введеных чисел\n\nexit - выход\n");
-
-            switch (Console.ReadLine())
-            {
-                case "add":
-                    Console.Write("Введите число : ");
-                    numbers.Add(Convert.ToInt32(Console.ReadLine()));
-                    break;
-                case "sum":
-                    for (int i = 0; i < numbers.Count; i++)
-                    {
-                        sum += numbers[i];
-                    }
-                    Console.WriteLine(sum);
-                    break;
-                case "exit":
-                    enterOrExit = false;
-                    break;
-            }
-            Console.WriteLine("Нажмите любую клавишу для продолжения...");
-            Console.ReadKey();
-            Console.Clear();
-        }
+        }        
     }
 }
